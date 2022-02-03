54efc05dd62243736689e7f7538074440d5f766c
zss <1046686548@qq.com>
2022-02-03 13:14:26 +0800

    增加了输出信息

--- TestModule/src/com/zss/Student.java
+++ TestModule/src/com/zss/Student.java
@@ -4,5 +4,6 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("nihao");
         System.out.println("你好");
+        System.out.println("nnnn");
     }
 }
